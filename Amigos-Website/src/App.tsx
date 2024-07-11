e36ece6fd25022550d9a5a7661a13f6ee1d02661
Samra <your_email@example.com>
2024-07-11 11:57:31 -0700

    installed chakra ui

--- Amigos-Website/src/App.tsx
+++ Amigos-Website/src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <h1>hello amigos</h1>;
+  return (
+    <h1>
+      <Button colorScheme="blue">Button</Button>
+    </h1>
+  );
 }
 
 export default App;
