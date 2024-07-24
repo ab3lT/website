7cd6c978d300cab3754699108309153c3d93a9ad
Abel <se.abel.tadesse@gmail.com>
2024-07-24 12:03:24 +0300

    one error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, Heading, Link } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTelegram,
